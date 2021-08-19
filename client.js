2835cd2f495400b34218e6a082a01717d9628f34
Neha Yadav <nehayadav903@gmail.com>
2021-08-19 16:19:14 -0400

    Update the connect function

---
+++ client.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+ const connect = function() {
+ const conn = net.createConnection({
+  host: 'localhost',
+  port: 50541
+});
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  conn.on('connect', () => {
+    conn.write('Hello from client!');
+  });
+// interpret incoming data as text
+conn.setEncoding('utf8');
+
+return conn;
+}
+
+process.stdout.write('\x07');
+
+const { Game } = require('./src/Game')
+const { UserInterface } = require('./src/UserInterface')
+const { RemoteInterface } = require('./src/RemoteInterface')
+const game = new Game(new UserInterface(), new RemoteInterface())
+
+// Begin game
+game.start()
+
+ module.exports = { connect };
