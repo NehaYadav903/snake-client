a291f9edacb34ed1a2a44c519419f75e001afb19
Neha Yadav <nehayadav903@gmail.com>
2021-08-20 00:22:56 -0400

    Updated the input module to send these strings to the server using the connection object.

--- input.js
+++ input.js
@@ -1,19 +1,33 @@
-
- const setupInput = function() {
-  const stdin = process.stdin;
+let connection;
+ //const setupInput = function() {
+  const setupInput = function(conn) {
+    connection = conn;
+ const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  const handleUserInput = function(key) {
-    if (key === '\u0003') { // \u0003 maps to ctrl+c input
-      console.log('exiting now');
-      process.exit();
-    }
-  };
   stdin.on('data', (key) => { // on data input/key being pushed, call handleUserInput with the key being pushed
     handleUserInput(key);
   });
   return stdin;
-}
-
+};
+setupInput(); // calling function
+const handleUserInput = (key) => { // once called by key, execute this block
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input
+    console.log('exiting now...');
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+};
 module.exports = { setupInput };
